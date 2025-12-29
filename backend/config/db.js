722b94a795a34d0ce4d3ad38173736a0b08ac574
shruti576 <shrutirawat508@gmail.com>
2025-12-29 21:03:35 +0530

    db code correction

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,16 @@
-// mongodb+srv://delivery:f09oo#d@cluster0.wgtskmg.mongodb.net/?
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config(); 
 
 export const connectDB = async()=>{
-    await mongoose.connect(process.env.MONGO_URI)
-    .then(()=>console.log("db connected"));
+    // await mongoose.connect(process.env.MONGO_URI)
+    // .then(()=>console.log("db connected"));
+     try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+    process.exit(1); // stop server if DB fails
+  }
 }
