bafda4695b65c3564a7533bbe2e8447f61227883
Shruti <shrutirawat508@gmail.com>
2026-03-01 17:23:39 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,5 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
+// export const url = 'http://localhost:4000'
+export const url = 'https://food-delivery-48tq.onrender.com'
